5bf2dfb16bf6842f6890cb97e9f7395b47363330
Jim Pick <jim@jimpick.com>
2021-02-10 18:02:45 -0800

    Basic packaging for frontend code

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World 2!')
+const baseDir = path.join(__dirname, 'earthstar-foyer')
+const publicDir = path.join(baseDir, 'public')
+const staticDir = path.join(baseDir, 'public/static')
+const indexPath = path.join(publicDir, 'index.html')
+console.log('public dir:', publicDir)
+console.log('static dir:', staticDir)
+console.log('index path:', indexPath)
+
+app.use('/static', express.static(staticDir))
+
+// serve the index path for any URL
+app.get('*', (request, response) => {
+  response.sendFile(indexPath)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const listener = app.listen(port, function () {
+  console.log(`Your app is listening on port ${listener.address().port}`)
 })
